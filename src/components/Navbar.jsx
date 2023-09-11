5b3d682f2a2987e468adc1d20f35dadf6f07701e
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-09-11 15:39:23 +0200

    logo a oprava tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+
+import { logoLu2, menu, close } from "../assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState("");
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logoLu2} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Ing.Petr Ludvik <span className="sm:block hidden">- Programátor robotů</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
