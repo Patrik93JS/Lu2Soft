09b21fd382eddc21b759efa9e42854fb90ae8c91
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-09-14 11:21:47 +0200

    pred 3d zmenou

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,9 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  // const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./robot/scene.gltf");
+
   return (
     <mesh>
       <hemisphereLight intensity={1.15} groundColor="black" />
