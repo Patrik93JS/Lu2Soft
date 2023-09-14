1b0b2af3073008c72887f7ce683cecdc18d4bf14
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-09-14 11:43:48 +0200

    robot 3D

--- src/components/canvas/Robot.jsx
+++ src/components/canvas/Robot.jsx
@@ -4,33 +4,32 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
-  const computer = useGLTF("./robot/scene.gltf");
+const Robot = ({ isMobile }) => {
+  const robot = useGLTF("./robot/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.15} groundColor="black" />
+      <hemisphereLight intensity={3.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
+        angle={1.12}
+        penumbra={10}
+        intensity={10}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        object={robot.scene}
+        scale={isMobile ? 0.7 : 1.3}
+        position={isMobile ? [0, -3, -2.2] : [-10, -100.25, -10]}
+        rotation={[0, -0.2, 0]}
       />
     </mesh>
   );
 };
 
-const ComputersCanvas = () => {
+const RobotCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -56,7 +55,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [500, 500, 500], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -65,7 +64,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Robot isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
@@ -73,4 +72,4 @@ const ComputersCanvas = () => {
   );
 };
 
-export default ComputersCanvas;
+export default RobotCanvas;
